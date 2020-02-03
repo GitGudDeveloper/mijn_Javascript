3ee697bb57c9615bd32d8a782fafb7b805019bbe
Quincy van Kempen <2113342@talnet.nl>
2020-02-03 12:56:10 +0100

    Initial commit

---
+++ h02/opdracht2.1.js
@@ -0,0 +1,6 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("kopje").innerText = "Dit is de subkop";
+document.getElementById("plaatje1").src = "meliodas.jpg";
+document.getElementById("plaatje2").src = "meliodas_demonform.jpg";
+document.getElementById("plaatje2").style.width = "400px";
+document.getElementById("plaatje2").style.height = "224px";
---
+++ h02/opdracht2.2.js
@@ -0,0 +1,55 @@
+console.log("dit is opdracht 2.2");
+var body = document.getElementById("body");
+var blok_1 = document.getElementById("blok_1");
+var tekst_blokje_1 = document.getElementById("tekst_blokje_1");
+var blok_2 = document.getElementById("blok_2");
+var tekst_blokje_2 = document.getElementById("tekst_blokje_2");
+var blok_3 = document.getElementById("blok_3");
+var tekst_blokje_3 = document.getElementById("tekst_blokje_3");
+var blok_4 = document.getElementById("blok_4");
+var tekst_blokje_4 = document.getElementById("tekst_blokje_4");
+var blokken = document.getElementsByClassName("blokken");
+body.style.height = "800px";
+{
+    blok_1.style.width = "250px";
+    blok_1.style.height = "300px";
+    blok_1.style.backgroundColor = "aqua";
+    blok_1.style.float = "left";
+    blok_1.style.border = "none";
+    blok_1.style.textAlign = "center";
+    tekst_blokje_1.style.marginTop = "50px";
+}
+{
+    blok_2.style.width = "250px";
+    blok_2.style.height = "300px";
+    blok_2.style.backgroundColor = "green";
+    blok_2.style.float = "left";
+    blok_2.style.border = "none";
+    tekst_blokje_2.style.marginTop = "0px";
+    blok_2.style.marginTop = "50px";
+    tekst_blokje_2.style.textAlign = "center";
+}
+{
+    blok_3.style.backgroundColor = "yellow";
+    blok_3.style.width = "250px";
+    blok_3.style.height = "300px";
+    blok_3.style.float = "left";
+    blok_3.style.border = "none";
+    tekst_blokje_3.style.marginTop = "0px";
+    tekst_blokje_3.style.textAlign = "right";
+    blok_3.style.marginTop = "130px";
+}
+{
+    blok_4.style.backgroundColor = "red";
+    blok_4.style.width = "250px";
+    blok_4.style.height = "300px";
+    blok_4.style.padding = "0px";
+    blok_4.style.margin = "0px";
+    blok_4.style.float = "left";
+    blok_4.style.border = "none";
+    tekst_blokje_4.style.marginTop = "0px";
+}
+
+
+
+
